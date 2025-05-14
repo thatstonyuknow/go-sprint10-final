fee8deabb8ab07754441160843cd83a94ac6b610
Anton Timoshenko <root@root.com>
2025-05-14 19:41:45 +0300

    [SPRINT10] enhanced mkdir command

--- precode.sh
+++ precode.sh
@@ -6,7 +6,7 @@
 #   dir2
 #   dir3
 #       dir4
-mkdir -p task/dir1 && mkdir task/dir2 && mkdir -p task/dir3/dir4
+mkdir -p task/dir1 task/dir2 task/dir3/dir4
 
 # изменяем текущую директорию на task
 cd task
